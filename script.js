c43321a24277f6318913b4faca94f32bdd95a196
Ahmed-Elzayat112 <ahmedelzayat397@gmail.com>
2024-02-18 21:24:52 +0200

    add project

---
+++ script.js
@@ -0,0 +1,16 @@
+const textareas = document.querySelectorAll("textarea");
+
+function run() {
+    const htmlCode = document.getElementById("html-code").value;
+    const cssCode = document.getElementById("css-code").value;
+    const jsCode = document.getElementById("js-code").value;
+    const outPut = document.getElementById("output");
+
+    outPut.contentDocument.body.innerHTML =
+        htmlCode + "<style>" + cssCode + "</style>";
+    outPut.contentWindow.eval(jsCode);
+}
+
+textareas.forEach((item) => {
+    item.addEventListener("keyup", run);
+});
